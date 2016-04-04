74224f3aadd791ff8b636eb93e7cf67f23408ba8
Simon Legner <Simon.Legner@gmail.com>
2016-04-04 16:29:03 +0200

    divIcon: make transcluding robuster

--- src/directives/divIcon.js
+++ src/directives/divIcon.js
@@ -16,7 +16,10 @@
     function link(scope, element, attrs, llMarker, transclude) {
       transclude(function(html) {
         var options = scope.llDivIcon || {};
-        options.elm = html[0];
+        options.elm = html.filter(function() {
+          /* eslint-env browser */
+          return this.nodeType !== Node.TEXT_NODE;
+        }).first()[0];
         var icon = createIcon(options);
         llMarker.getMarker().then(function(marker) {
           marker.setIcon(icon);
@@ -28,6 +31,7 @@
       var MyIcon = L.Icon.extend({
         options: {},
         createIcon: function() {
+          this.options.elm = this.options.elm || document.createElement('div');
           this._setIconStyles(this.options.elm, 'icon');
           return this.options.elm;
         }
